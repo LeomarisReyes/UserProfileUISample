e25f75aab5034193166e62babf2db402438ecf5b
Sebastian Jaschinski <jaschinski@it-freelancer.de>
2021-03-17 12:31:08 +0100

    Refactored to clickable buttons
    Refactored solution to clickable buttons by removing translates, changing grid layouts and order of elements in XAML to influence z-index.

--- UserProfileUISample/MainPage.xaml.cs
+++ UserProfileUISample/MainPage.xaml.cs
@@ -1,21 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
 
 namespace UserProfileUISample
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void PhoneClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Contact", "Phone clicked!", "OK");
+        }
+
+        private async void MailClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Contact", "Mail clicked!", "OK");
+        }
+
+        private async void FollowClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Follow", "Follow clicked!", "OK");
         }
     }
 }
